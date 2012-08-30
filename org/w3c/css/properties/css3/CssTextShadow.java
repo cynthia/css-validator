99690f8f1507342d756c8a50535c932928eb3260
ylafon <yves@w3.org>
2012-08-30 17:03:34 +0000

    color can be last... or first

--- org/w3c/css/properties/css3/CssTextShadow.java
+++ org/w3c/css/properties/css3/CssTextShadow.java
@@ -1,4 +1,4 @@
-// $Id: CssTextShadow.java,v 1.1 2012-08-29 10:09:51 ylafon Exp $
+// $Id: CssTextShadow.java,v 1.2 2012-08-30 17:03:34 ylafon Exp $
 // Author: Yves Lafon <ylafon@w3.org>
 //
 // (c) COPYRIGHT MIT, ERCIM and Keio University, 2012.
@@ -120,13 +120,9 @@ public class CssTextShadow extends org.w3c.css.properties.css.CssTextShadow {
 		while (!exp.end()) {
 			val = exp.getValue();
 			// color is last, so if we reach this, we are in error
-			if (color != null) {
-				throw new InvalidParamException("value",
-						val, getPropertyName(), ac);
-			}
 			if (val.getType() == CssTypes.CSS_NUMBER) {
 				// case of 0, a number and a length
-				val = ((CssNumber)val).getLength();
+				val = ((CssNumber) val).getLength();
 			}
 			if (val.getType() == CssTypes.CSS_LENGTH) {
 				values.add(val);
@@ -134,6 +130,13 @@ public class CssTextShadow extends org.w3c.css.properties.css.CssTextShadow {
 			} else {
 				CssColor c = new CssColor(ac, exp, false);
 				color = c.getColor();
+				// color can be first or last in CSS2
+				if (values.size() > 0 && exp.getRemainingCount() != 0) {
+					if (color != null) {
+						throw new InvalidParamException("value",
+								val, getPropertyName(), ac);
+					}
+				}
 				// no need for exp.next() as CssColor parsing is
 				// already doing that.
 			}
