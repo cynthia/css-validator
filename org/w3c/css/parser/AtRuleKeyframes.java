83322bc72d05b8556e0fe91a8d709fb5f1422a70
ylafon <yves@w3.org>
2012-10-10 13:55:30 +0000

    checking selector value

--- org/w3c/css/parser/AtRuleKeyframes.java
+++ org/w3c/css/parser/AtRuleKeyframes.java
@@ -1,11 +1,44 @@
-// $Id: AtRuleKeyframes.java,v 1.1 2012-10-10 07:47:30 ylafon Exp $
+// $Id: AtRuleKeyframes.java,v 1.2 2012-10-10 13:55:30 ylafon Exp $
 // Author: Yves Lafon <ylafon@w3.org>
 //
 // (c) COPYRIGHT MIT, ERCIM and Keio University, 2012.
 // Please first read the full copyright statement in file COPYRIGHT.html
 package org.w3c.css.parser;
 
+import org.w3c.css.util.ApplContext;
+import org.w3c.css.util.InvalidParamException;
+import org.w3c.css.values.CssIdent;
+import org.w3c.css.values.CssPercentage;
+import org.w3c.css.values.CssTypes;
+import org.w3c.css.values.CssValue;
+
 public class AtRuleKeyframes extends AtRule {
+
+	static final CssIdent to, from;
+
+	static {
+		to = CssIdent.getIdent("to");
+		from = CssIdent.getIdent("from");
+	}
+
+	public static void checkSelectorValue(CssValue selector, ApplContext ac)
+			throws InvalidParamException {
+		switch (selector.getType()) {
+			case CssTypes.CSS_PERCENTAGE:
+				CssPercentage percentage = selector.getPercentage();
+				if (!percentage.isPositive() || percentage.floatValue() > 100.f) {
+					 throw new InvalidParamException("range", ac);
+				}
+				break;
+			case CssTypes.CSS_IDENT:
+				CssIdent id = (CssIdent) selector;
+				if (to.equals(id) || from.equals(id)) {
+					break;
+				}
+			default:
+				throw new InvalidParamException("selectorname", selector.toString(), ac);
+		}
+	}
 
 	String name = null;
 
@@ -38,6 +71,7 @@ public class AtRuleKeyframes extends AtRule {
 	public String lookupPrefix() {
 		return "";
 	}
+
 	/**
 	 * Returns a string representation of the object.
 	 */
