1c29e8512b46c5fbb299b6711f78a4db5ee1228c
ylafon <yves@w3.org>
2012-12-06 19:11:19 +0000

    removing column-span form css2/21/atsc and row-span

--- org/w3c/css/properties/css2/table/Css2Style.java
+++ org/w3c/css/properties/css2/table/Css2Style.java
@@ -1,5 +1,5 @@
 //
-// $Id: Css2Style.java,v 1.8 2012-12-06 18:58:14 ylafon Exp $
+// $Id: Css2Style.java,v 1.9 2012-12-06 19:11:19 ylafon Exp $
 // From Philippe Le Hegaret (Philippe.Le_Hegaret@sophia.inria.fr)
 //
 // (c) COPYRIGHT MIT and INRIA, 1997.
@@ -9,55 +9,12 @@
 package org.w3c.css.properties.css2.table;
 
 /**
- * @version $Revision: 1.8 $
+ * @version $Revision: 1.9 $
  */
 public class Css2Style extends org.w3c.css.properties.css2.Css2Style {
 
-    RowSpan rowSpan;
-    RowSpanATSC rowSpanATSC;
-    ColumnSpan columnSpan;
-    ColumnSpanATSC columnSpanATSC;
     SpeakHeader speakHeader;
     SpeakHeaderATSC speakHeaderATSC;
-
-    /**
-     * Get the row-span property
-     */
-    public final RowSpan getRowSpan() {
-	if (rowSpan == null) {
-	    rowSpan = (RowSpan)
-		style.CascadingOrder(new RowSpan(), style, selector);
-	}
-	return rowSpan;
-    }
-
-    public final RowSpanATSC getRowSpanATSC() {
-	if (rowSpanATSC == null) {
-	    rowSpanATSC = (RowSpanATSC)
-		style.CascadingOrder(new RowSpanATSC(), style, selector);
-	}
-	return rowSpanATSC;
-    }
-
-    /**
-     * Get the column-span property
-     */
-
-    public final ColumnSpan getColumnSpan() {
-	if (columnSpan == null) {
-	    columnSpan = (ColumnSpan)
-		style.CascadingOrder(new ColumnSpan(), style, selector);
-	}
-	return columnSpan;
-    }
-
-    public final ColumnSpanATSC getColumnSpanATSC() {
-	if (columnSpanATSC == null) {
-	    columnSpanATSC = (ColumnSpanATSC)
-		style.CascadingOrder(new ColumnSpanATSC(), style, selector);
-	}
-	return columnSpanATSC;
-    }
 
     /**
      * Get the emtpy-header property
