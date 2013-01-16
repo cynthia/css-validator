b0493721a3851fffb10c77902643c5c362854b80
Alexandre Bertails <bertails@gmail.com>
2013-01-16 17:13:20 -0500

    use sbt as build tool

---
+++ project/Build.scala
@@ -0,0 +1,82 @@
+import sbt._
+import sbt.Keys._
+
+/**
+ * build configuration for css-validator
+ */
+object CssValidatorBuild extends Build {
+
+  lazy val getMissingJars = TaskKey[Unit]("get-missing-jars", "Get missing jars")
+
+  val jettyVersion = "8.1.7.v20120910"
+  val javaServletVersion = "3.0.0.v201112011016"
+
+  lazy val cssValidator = Project(
+    id = "validator",
+    base = file("."),
+
+    settings = Defaults.defaultSettings ++ Seq(
+      organization := "org.w3",
+      version := "1.0-SNAPSHOT",
+      scalaVersion := "2.10.0",
+      libraryDependencies += "commons-collections" % "commons-collections" % "3.2.1",
+      libraryDependencies += "commons-lang" % "commons-lang" % "2.6",
+      libraryDependencies += "org.apache.velocity" % "velocity" % "1.7",
+      libraryDependencies += "tagsoup" % "tagsoup" % "1.2" from "http://home.ccil.org/~cowan/XML/tagsoup/tagsoup-1.2.jar",
+      libraryDependencies += "org.eclipse.jetty" % "jetty-webapp" % jettyVersion % "compile" intransitive(),
+      libraryDependencies += "org.eclipse.jetty" % "jetty-util" % jettyVersion % "compile" intransitive(),
+      libraryDependencies += "org.eclipse.jetty" % "jetty-server" % jettyVersion % "compile" intransitive(),
+      libraryDependencies += "org.eclipse.jetty" % "jetty-io" % jettyVersion % "compile" intransitive(),
+      libraryDependencies += "org.eclipse.jetty" % "jetty-http" % jettyVersion % "compile" intransitive(),
+      libraryDependencies += "org.eclipse.jetty" % "jetty-security" % jettyVersion % "compile" intransitive(),
+      libraryDependencies += "org.eclipse.jetty" % "jetty-continuation" % jettyVersion % "compile" intransitive(),
+      libraryDependencies += "org.eclipse.jetty" % "jetty-xml" % jettyVersion % "compile" intransitive(),
+      libraryDependencies += "org.eclipse.jetty" % "jetty-servlet" % jettyVersion % "compile" intransitive(),
+      libraryDependencies += "org.eclipse.jetty" % "jetty-servlets" % jettyVersion % "compile" intransitive(),
+      libraryDependencies += "org.eclipse.jetty.orbit" % "javax.servlet" % javaServletVersion % "compile" artifacts (Artifact("javax.servlet", "jar", "jar")) intransitive(),
+      ivyXML :=
+        <dependency org="org.eclipse.jetty.orbit" name="javax.servlet" rev={ javaServletVersion }>
+          <artifact name="javax.servlet" type="orbit" ext="jar"/>
+        </dependency>,
+
+      getMissingJars := {
+        import java.net.URL
+        import java.io.File
+        val tmp = new File(System.getProperty("java.io.tmpdir"))
+        val lib = new File("lib")
+        if (! (lib / "xercesImpl.jar").exists) {
+          val url = new URL("http://www.apache.org/dist/xerces/j/binaries/Xerces-J-bin.2.11.0.zip")
+          lib.mkdir()
+          val files = IO.unzipURL(from = url, toDirectory = tmp, filter = "xerces-2_11_0/xercesImpl.jar" | "xerces-2_11_0/xml-apis.jar")
+          files foreach { file =>
+            println("moving \"" + file.getAbsolutePath + "\" to \"" + lib.getAbsolutePath + "\"")
+            IO.move(file, lib / file.getName)
+          }
+        }
+        if (! (lib / "htmlparser-1.3.1.jar").exists) {
+          val url = new URL("http://about.validator.nu/htmlparser/htmlparser-1.3.1.zip")
+          lib.mkdir()
+          val files = IO.unzipURL(from = url, toDirectory = tmp, filter = "htmlparser-1.3.1/htmlparser-1.3.1.jar")
+          files foreach { file =>
+            println("moving \"" + file.getAbsolutePath + "\" to \"" + lib.getAbsolutePath + "\"")
+            IO.move(file, lib / file.getName)
+          }
+        }
+        if (! (lib / "jigsaw.jar").exists) {
+          val url = new URL("http://jigsaw.w3.org/Distrib/jigsaw_2.2.6.zip")
+          lib.mkdir()
+          val files = IO.unzipURL(from = url, toDirectory = tmp, filter = "Jigsaw/classes/jigsaw.jar")
+          files foreach { file =>
+            println("moving \"" + file.getAbsolutePath + "\" to \"" + lib.getAbsolutePath + "\"")
+            IO.move(file, lib / file.getName)
+          }
+        }
+
+
+      }
+
+
+    )
+  )
+
+}
